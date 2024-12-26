ff4d6296622ef762b0e2aedd9aa873903d1c2748
HRUSHIKESH DOKALA <96101829+Hk669@users.noreply.github.com>
2024-12-26 14:50:31 +0000

    ReasoningAgent benchmarking with SimpleBench

---
+++ benchmark/run_simple-bench.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2023 - 2024, Owners of https://github.com/ag2ai
+#
+# SPDX-License-Identifier: Apache-2.0
+import json
+import re
+
+from autogen import ReasoningAgent, ThinkNode, UserProxyAgent
+
+config_list = [{"model": "gpt-4o-mini", "api_key": "api-key"}]
+
+
+def load_data(file_name):
+    with open(file_name, "r") as f:
+        return json.load(f)
+
+
+def run_benchmark(eval_list):
+
+    reasoning_agent = ReasoningAgent(
+        name="reasoning_agent",
+        llm_config={"config_list": config_list},
+        reason_config={"method": "lats", "nsim": 2, "max_depth": 3},
+    )
+
+    user_proxy = UserProxyAgent(
+        name="user_proxy",
+        human_input_mode="NEVER",
+        code_execution_config=False,
+    )
+
+    results = []
+    summaries = []
+
+    for i, eval_set in enumerate(eval_list):
+        eval_question = eval_set["prompt"]
+        question_id = eval_set["question_id"]
+        answer = eval_set["answer"]
+        prompt = """Give the final answer using the following format: `Final Answer: X` where X is one of the letters A, B, C, D, E, or F."""
+
+        question = eval_question + prompt
+
+        ans = user_proxy.initiate_chat(reasoning_agent, message=question, summary_method=last_meaningful_msg)
+        summary = ans.summary
+        summaries.append(summary + "\n\n\n")
+        match = re.search(r"Final Answer: ([A-F])", summary)
+
+        if match:
+            extracted_answer = match.group(1)
+            results.append({"question_id": question_id, "answer": answer, "generated_answer": extracted_answer})
+        else:
+            print("No Final Answer found in the response.")
+            results.append(
+                {
+                    "question_id": question_id,
+                    "answer": answer,
+                    "generated_answer": "No Final Answer found in the response.",
+                }
+            )
+
+        data = reasoning_agent._root.to_dict()
+        with open(f"reasoning_tree{i}.json", "w") as f:
+            json.dump(data, f)
+
+    with open("results-lats.json", "w") as f:
+        json.dump(results, f, indent=4)
+
+    with open("summaries.txt", "w") as f:
+        f.writelines(summaries)
+
+
+def last_meaningful_msg(sender, recipient, summary_args):
+    import warnings
+
+    if sender == recipient:
+        return "TERMINATE"
+
+    summary = ""
+    chat_messages = recipient.chat_messages[sender]
+
+    for msg in reversed(chat_messages):
+        try:
+            content = msg["content"]
+            if isinstance(content, str):
+                summary = content.replace("TERMINATE", "")
+            elif isinstance(content, list):
+                # Remove the `TERMINATE` word in the content list.
+                summary = "\n".join(
+                    x["text"].replace("TERMINATE", "") for x in content if isinstance(x, dict) and "text" in x
+                )
+            if summary.strip().rstrip():
+                return summary
+        except (IndexError, AttributeError) as e:
+            warnings.warn(f"Cannot extract summary using last_msg: {e}. Using an empty str as summary.", UserWarning)
+    return summary
+
+
+if __name__ == "__main__":
+    data = load_data("data.json")
+    run_benchmark(data["eval_data"])
