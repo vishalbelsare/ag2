61550c7de40c274aa9df98aa75c30ebe8d3e04fb
Mohamed Abdelbary <mo@occami.ai>
2025-03-04 23:00:13 +0000

    wip OccamAgent integration with Occam SDK

--- autogen/agents/experimental/occam/occam.py
+++ autogen/agents/experimental/occam/occam.py
@@ -3,44 +3,80 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+import os
 from typing import Optional, Union
 
-from .... import Agent, ConversableAgent, OpenAIWrapper
-from ....doc_utils import export_module
-from ....io.base import IOStream
-from ....messages.agent_messages import (
+from autogen.agentchat import Agent, ConversableAgent
+from autogen.oai import OpenAIWrapper
+from autogen.doc_utils import export_module
+from autogen.io.base import IOStream
+from autogen.messages.agent_messages import (
     TerminationAndHumanReplyMessage,
 )
+
+from occam_core.agents.model import AgentIOModel, OccamLLMMessage
+from occamai.api_client import OccamClient
+from occamai.api_client import AgentInstanceParamsModel, AgentRunDetail
+
 
 __all__ = ["OccamAgent"]
 
 
 @export_module("autogen.agents")
 class OccamAgent(ConversableAgent):
-    """Occam.ai Agent"""
+    def _convert_ag_messages_to_agent_io_model(self, messages: list[dict]) -> AgentIOModel:
+        return AgentIOModel(
+            chat_messages=[
+                OccamLLMMessage(
+                    role=message["role"],
+                    content=message["content"]
+                )
+                for message in messages
+            ]
+        )
 
-    def my_reply_func(
+    def occam_reply_func(
         self,
         messages: Optional[list[dict]] = None,
         sender: Optional[Agent] = None,
         config: Optional[OpenAIWrapper] = None,
     ) -> tuple[bool, Union[str, dict, None]]:
-        # Do all your processing in here.
+        """
+        TODO:
+        - Support path of resume, can do by ping before running the agent.
+        - Some errors should be fashioned as communication, not raise errors, e.g. out of quota.
+        Auto-resume considerations:
+        * Auto-resume when topped up after an auto-pause, we don't have control over the workflow that the agent is running within.
+        * Don't auto-resume and rely on user to always resume.
+        """
 
-        # Loop and wait for status to be completed or failed, etc.
+        # TODO: Convert messages to AgentIOModel
+        agent_io_model = self._convert_ag_messages_to_agent_io_model(messages)
+
+        agent_run_detail: AgentRunDetail = self.occam_client.agents.run_agent(
+            agent_instance_id=self.occam_agent_instance_id,
+            sync=True,
+            agent_input_model=agent_io_model,
+        )
+        print(f"Agent run status: {agent_run_detail.status}")
+        print(f"Agent run result: {agent_run_detail.result}")
+
+        agent_output = agent_run_detail.result
 
         # Send a message to the user to notify them it's waiting. Create a message as needed
-        iostream = IOStream.get_default()
-        iostream.send(
-            TerminationAndHumanReplyMessage(no_human_input_msg="Dummy message", sender=sender, recipient=self)
-        )
+        # iostream = IOStream.get_default()
+        # iostream.send(
+        #     TerminationAndHumanReplyMessage(no_human_input_msg="Dummy message", sender=sender, recipient=self)
+        # )
 
         # True indicates final reply, string goes back into the chat's messages.
-        return True, "Message from Occam Agent to go back into workflow."
+        return True, "".join([m.content for m in agent_output.chat_messages])
 
     def __init__(
         self,
-        agent_params,
+        api_key: str,
+        agent_params: Optional[AgentInstanceParamsModel] = AgentInstanceParamsModel(),
+        base_url: str = "https://api.occam.ai",
         *args,
         **kwargs,
     ) -> None:
@@ -51,9 +87,45 @@ class OccamAgent(ConversableAgent):
         """
 
         super().__init__(*args, **kwargs)
+        self.occam_client = OccamClient(api_key=api_key, base_url=base_url)
+
+        # Initialise agent.
+        occam_agent_instance = self.occam_client.agents.instantiate_agent(
+            agent_name="DeepSeek: R1 Distill Llama 70B",
+            agent_params=agent_params
+        )
+        self.occam_agent_instance_id = occam_agent_instance.agent_instance_id
+        print(f"Created Occam Agent instance: {self.occam_agent_instance_id}")
+
+        # TODO: Remove below code, it's in the reply_func
+        # agent_run_detail: AgentRunDetail = self.occam_client.agents.run_agent(
+        #     agent_instance_id=self.occam_agent_instance_id,
+        #     sync=True,
+        #     agent_input_model=AgentIOModel(
+        #         chat_messages=[
+        #             OccamLLMMessage(
+        #                 role="user",
+        #                 content="Hello, tell me an interesting fact about the moon."
+        #             )
+        #         ]
+        #     )
+        # )
+        # print(f"Agent run status: {agent_run_detail.status}")
+        # print(f"Agent run result: {agent_run_detail.result}")
 
         self.register_reply(
             trigger=[Agent, None],
-            reply_func=self.my_reply_func,
+            reply_func=self.occam_reply_func,
             remove_other_reply_funcs=True,
         )
+
+
+if __name__ == "__main__":
+    api_key = os.getenv("OCCAM_API_KEY")
+    base_url = os.getenv("OCCAM_BASE_URL")
+
+    agent = OccamAgent(api_key=api_key, base_url=base_url, name="occam-agent")
+    agent.initiate_chat(
+        recipient=agent,
+        message="Hello, tell me an interesting fact about the moon."
+    )
